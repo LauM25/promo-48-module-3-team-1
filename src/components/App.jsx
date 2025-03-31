7459ed4f5a235d7047bc2fe61f5be5c3192d6439
Irmitate <irmitate@gmail.com>
2025-03-31 12:42:05 +0100

    refactory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,46 +5,90 @@ import Footer from "./Footer";
 import Form from "./Form";
 import Preview from "./Preview";
 
-
-
-
 //inputs
 
 function App() {
-  const [projectName, setProjectName] = useState("Elegant Workspace");
+  /*const [projectName, setProjectName] = useState("Elegant Workspace");
   const [projectSlogan, setProjectSlogan] = useState("Diseños exclusivos");
   const [projectRepository, setProjectRepository] = useState("");
   const [projectDemo, setProjectDemo] = useState("");
-  const [projectTechnologies, setProjectTechnologies] = useState("React JS - HTML - CSS");
-  const [projectDescription, setProjectDescription] = useState("Tiene musho peligro caballo blanco caballo negroorl te voy a borrar el cerito está la cosa muy malar qué dise usteer llevame al sircoo.");
+  const [projectTechnologies, setProjectTechnologies] = useState(
+    "React JS - HTML - CSS"
+  );
+  const [projectDescription, setProjectDescription] = useState(
+    "Tiene musho peligro caballo blanco caballo negroorl te voy a borrar el cerito está la cosa muy malar qué dise usteer llevame al sircoo."
+  );
   const [userName, setUserName] = useState("Emmelie Bjôrklund");
   const [userJob, setUserJob] = useState("Full stack Developer");
+  */
+
+  const [projectInfo, setProjectInfo] = useState({
+    projectName: "Elegant Workspace",
+    projectSlogan: "Diseños exclusivos",
+    projectDescription:
+      "Tiene musho peligro caballo blanco caballo negroorl te voy a borrar el cerito está la cosa muy malar qué dise usteer llevame al sircoo.",
+    projectRepository: "",
+    projectDemo: "",
+    projectTechnologies: "React JS - HTML - CSS",
+    userName: "Emmelie Bjôrklund",
+    userJob: "Full stack Developer",
+  });
 
   const handleChangeProjectName = (value) => {
-    setProjectName(value);
- console.log(value);
+    setProjectInfo({
+      ...projectInfo,
+      projectName: value,
+    });
   };
+
   const handleChangeProjectSlogan = (value) => {
-    setProjectSlogan(value);
+    setProjectInfo({
+      ...projectInfo,
+      projectSlogan: value,
+    });
   };
+
   const handleChangeProjectRepository = (value) => {
-    setProjectRepository(value);
+    setProjectInfo({
+      ...projectInfo,
+      projectRepository: value,
+    });
   };
+
   const handleChangeProjectDemo = (value) => {
-    setProjectDemo(value);
+    setProjectInfo({
+      ...projectInfo,
+      projectDemo: value,
+    });
   };
+
   const handleChangeProjectTechnologies = (value) => {
-    setProjectTechnologies(value);
+    setProjectInfo({
+      ...projectInfo,
+      projectTechnologies: value,
+    });
   };
+
   const handleChangeProjectDescription = (value) => {
-    setProjectDescription(value);
+    setProjectInfo({
+      ...projectInfo,
+      projectDescription: value,
+    });
   };
+
   const handleChangeUserName = (value) => {
-    setUserName(value);
+    setProjectInfo({
+      ...projectInfo,
+      userName: value,
+    });
   };
   const handleChangeUserJob = (value) => {
-    setUserJob(value);
+    setProjectInfo({
+      ...projectInfo,
+      userJob: value,
+    });
   };
+
   return (
     <>
       <div className="container">
@@ -61,24 +105,17 @@ function App() {
             </a>
           </section>
 
-   <Preview
-   prName={projectName}
-   prSlogan={projectSlogan}
-   prRepository={projectRepository}
-   prDemo={projectDemo}
-   prTechnologies={projectTechnologies}
-   prDescription={projectDescription}
-   usName={userName}
-   usJob={userJob}/>
-          <Form 
-          onInputProjectName={handleChangeProjectName}
-          onInputProjectSlogan={handleChangeProjectSlogan}
-          onInputProjectRepository={handleChangeProjectRepository}
-          onInputProjectDemo={handleChangeProjectDemo}
-          onInputProjectTechnologies={handleChangeProjectTechnologies}
-          onInputProjectDescription={handleChangeProjectDescription}
-          onInputUserName={handleChangeUserName}
-          onInputUserJob={handleChangeUserJob}/>
+          <Preview project={projectInfo} />
+          <Form
+            onInputProjectName={handleChangeProjectName}
+            onInputProjectSlogan={handleChangeProjectSlogan}
+            onInputProjectRepository={handleChangeProjectRepository}
+            onInputProjectDemo={handleChangeProjectDemo}
+            onInputProjectTechnologies={handleChangeProjectTechnologies}
+            onInputProjectDescription={handleChangeProjectDescription}
+            onInputUserName={handleChangeUserName}
+            onInputUserJob={handleChangeUserJob}
+          />
         </main>
 
         <Footer />
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,61 +1,62 @@
-function Preview(props) {
+function Preview({ project }) {
+  //   console.log(project);
+  return (
+    <section className="preview">
+      <div className="projectImage"></div>
+      <article className="card">
+        <h2 className="card__projectTitle">
+          <span className="card__projectTitle--text">
+            Personal project card
+          </span>
+        </h2>
 
-    return (
-        <section className="preview">
-            <div className="projectImage"></div>
-            <article className="card">
-                <h2 className="card__projectTitle">
-                    <span className="card__projectTitle--text">
-                        Personal project card
-                    </span>
-                </h2>
+        <div className="card__author">
+          <div className="card__authorPhoto"></div>
+          <p className="card__job">
+            {project.userJob || "Full stack Developer"}
+          </p>
+          <h3 className="card__name">
+            {project.userName || "Emmelie Bjôrklund"}
+          </h3>
+        </div>
 
-                <div className="card__author">
-                    <div className="card__authorPhoto"></div>
-                    <p className="card__job">{props.usJob}</p>
-                    <h3 className="card__name">
-                        {props.usName}
-                    </h3>
-                </div>
+        <div className="card__project">
+          <h3 className="card__name">
+            {project.projectName || "Elegant Workspace"}
+          </h3>
+          <p className="card__slogan">
+            {project.projectSlogan || "Diseños exclusivos"}
+          </p>
+          <h3 className="card__descriptionTitle">Product Description</h3>
+          <p className="card__description">
+            {project.projectDescription ||
+              "Tiene musho peligro caballo blanco caballo negroorl te voy a borrar el cerito está la cosa muy malar qué dise usteer llevame al sircoo."}
+          </p>
 
-                <div className="card__project">
-                    <h3 className="card__name">
-                        {props.prName}
-                    </h3>
-                    <p className="card__slogan">
-                        {props.prSlogan}
-                    </p>
-                    <h3 className="card__descriptionTitle">Product Description</h3>
-                    <p className="card__description">
-                        {props.prDescription}
-                    </p>
+          <div className="card__technicalInfo">
+            <p className="card__technologies">
+              {project.projectTechnologies || "React JS - HTML - CSS"}
+            </p>
 
-                    <div className="card__technicalInfo">
-                        <p className="card__technologies">
-                            {props.prTechnologies}
-                        </p>
-
-                        <a
-                            className="icon icon__www"
-                            href={props.prDemo}
-                            title="Haz click para ver el proyecto online"
-                        >
-                            Web link
-                        </a>
-                        <a
-                            className="icon icon__github"
-                            href={props.prRepository}
-                            title="Haz click para ver el código del proyecto"
-                        >
-                            GitHub link
-                        </a>
-                    </div>
-                </div>
-            </article>
-        </section>
-    )
+            <a
+              className="icon icon__www"
+              href={project.projectDemo}
+              title="Haz click para ver el proyecto online"
+            >
+              Web link
+            </a>
+            <a
+              className="icon icon__github"
+              href={project.projectRepository}
+              title="Haz click para ver el código del proyecto"
+            >
+              GitHub link
+            </a>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
 }
-
-
 
 export default Preview;
